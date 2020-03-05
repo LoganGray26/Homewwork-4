80cf38381ccaf1dbcc7244c9b8d9f676c57c3cb1
logan Gray <ld.gray@setonhill.edu>
2020-03-04 19:53:43 -0500

    Initial Commit

---
+++ Homewwork #4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homewwork #4
+//
+//  Created by logan gray on 3/4/20.
+//  Copyright © 2020 logan gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
